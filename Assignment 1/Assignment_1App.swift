d1468bdb4919353c13249a9e33354214fdfd8d52
lcs-jdai <jdai@lcs.on.ca>
2021-11-06 10:14:01 -0400

    Initial Commit

---
+++ Assignment 1/Assignment_1App.swift
@@ -0,0 +1,17 @@
+//
+//  Assignment_1App.swift
+//  Assignment 1
+//
+//  Created by Jerry Dai on 2021-11-06.
+//
+
+import SwiftUI
+
+@main
+struct Assignment_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
