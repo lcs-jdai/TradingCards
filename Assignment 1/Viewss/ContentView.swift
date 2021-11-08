c8c885adae5b3c594ede82bedb16a16554268885
lcs-jdai <jdai@lcs.on.ca>
2021-11-07 21:25:38 -0500

    Finished the god lvmeng's first step.

---
+++ Assignment 1/Viewss/ContentView.swift
@@ -0,0 +1,37 @@
+//
+//  ContentView.swift
+//  Assignment 1
+//
+//  Created by Jerry Dai on 2021-11-06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ScrollView {
+            
+            ZStack {
+                Image ("lvmeg")
+                    .resizable()
+                    .scaledToFit()
+                Image ("god")
+                
+            }
+                 
+            
+            
+            
+            
+            
+            
+            
+            }
+        }
+    }
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
